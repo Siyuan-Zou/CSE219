ecd80530cbbbdea5bc8389e5dc2e830fb3338f11
Spirors <Spirors@10.1.189.152>
2018-01-29 11:28:12 -0500

    Initial commit of Recitation1 project.

--- Recitation1/src/recitation1/Recitation1.java
+++ Recitation1/src/recitation1/Recitation1.java
@@ -10,7 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 /**
@@ -30,9 +30,19 @@ public class Recitation1 extends Application {
                 System.out.println("Hello World!");
             }
         });
+        Button btn2 = new Button();
+        btn2.setText("Say 'Goodbye Cruel World!'");
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Goodbye Cruel World!");
+            }
+        });
         
-        StackPane root = new StackPane();
+        FlowPane root = new FlowPane();
         root.getChildren().add(btn);
+        root.getChildren().add(btn2);
         
         Scene scene = new Scene(root, 300, 250);
         
